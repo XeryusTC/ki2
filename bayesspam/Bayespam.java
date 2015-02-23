3f9f8293014c3b84274836551c8746659b6427e5
rmellema <rene.mellema@gmail.com>
2015-02-23 16:25:27 +0100

    Added the classification part

--- bayesspam/Bayespam.java
+++ bayesspam/Bayespam.java
@@ -14,8 +14,8 @@ public class Bayespam
     {
         int counter_spam    = 0;
         int counter_regular = 0;
-		float Pregular = 0;
-		float Pspam = 0;
+        float Pregular = 0;
+        float Pspam = 0;
 
         // Increase one of the counters by one
         public void incrementCounter(MessageType type)
@@ -35,7 +35,7 @@ public class Bayespam
     // A hash table for the vocabulary (word searching is very fast in a hash table)
     private static Hashtable <String, Multiple_Counter> vocab = new Hashtable <String, Multiple_Counter> ();
 
-	private static float Pregular, Pspam;
+    private static float Pregular, Pspam;
 
     // Add a word to the vocabulary
     private static void addWord(String word, MessageType type)
@@ -86,18 +86,18 @@ public class Bayespam
             counter  = vocab.get(word);
 
             System.out.println( word + " | in regular: " + counter.counter_regular +
-                                " in spam: "    + counter.counter_spam);
+                    " in spam: "    + counter.counter_spam);
         }
     }
 
     /// Read a file to a stream of tokens
-    private static String[] readFile(File f) {
-        ArrayList<String> ret = new ArrayList<String>();
+    private static String[] readFile(File f) throws IOException {
+        HashSet<String> ret = new HashSet<String>();
         FileInputStream in = new FileInputStream(f);
         BufferedReader buf = new BufferedReader(new InputStreamReader(in));
-        String line, word;
+        String line;
 
-        while((line = in.readLine()) != null) {
+        while((line = buf.readLine()) != null) {
             StringTokenizer tok = new StringTokenizer(line);
             while (tok.hasMoreTokens()) {
                 ret.add(tok.nextToken().replaceAll("[^a-zA-Z]", "").toLowerCase());
@@ -108,7 +108,7 @@ public class Bayespam
 
     // Read the words from messages and add them to your vocabulary. The boolean type determines whether the messages are regular or not
     private static void readMessages(MessageType type)
-    throws IOException
+        throws IOException
     {
         File[] messages = new File[0];
 
@@ -134,11 +134,11 @@ public class Bayespam
                 // while there are stille words left..
                 while (st.hasMoreTokens())
                 {
-					word = st.nextToken();
-					word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+                    word = st.nextToken();
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
                     // add them to the vocabulary
-					if (word.length() >= 4)
-	                    addWord(word, type);
+                    if (word.length() >= 4)
+                        addWord(word, type);
                 }
             }
 
@@ -147,9 +147,9 @@ public class Bayespam
     }
 
     public static void main(String[] args)
-    throws IOException
+        throws IOException
     {
-		float epsilon = 1;
+        float epsilon = 1;
         // Location of the directory (the path) taken from the cmd line (first arg)
         File dir_location = new File( args[0] );
 
@@ -173,47 +173,102 @@ public class Bayespam
         // Now all students must continue from here:
         //
         // 1) A priori class probabilities must be computed from the number of regular and spam messages
-		/// Count total words
-		int nMessagesRegular, nMessagesSpam, nMessagesTotal, nWordsRegular=0,
-			nWordsSpam=0;
-		
-		nMessagesRegular = listing_regular.length;
-		nMessagesSpam = listing_spam.length;
-		nMessagesTotal = nMessagesRegular + nMessagesSpam;
+        /// Count total words
+        int nMessagesRegular, nMessagesSpam, nMessagesTotal, nWordsRegular=0,
+            nWordsSpam=0;
 
-		Pregular = Math.log10(nMessagesRegular / (float)nMessagesTotal);
-		Pspam = Math.log10(nMessagesSpam / (float)nMessagesTotal);
+        nMessagesRegular = listing_regular.length;
+        nMessagesSpam = listing_spam.length;
+        nMessagesTotal = nMessagesRegular + nMessagesSpam;
 
-		System.out.println("P(regular):\t" + Pregular + "\nP(spam):\t" + Pspam);
-		
-		for (String it : vocab.keySet()) {
-			Multiple_Counter word = vocab.get(it);
-			nWordsRegular += word.counter_regular;
-			nWordsSpam    += word.counter_spam;
-		}
+        Pregular = (float)Math.log10(nMessagesRegular / (float)nMessagesTotal);
+        Pspam = (float)Math.log10(nMessagesSpam / (float)nMessagesTotal);
 
-		for (String it : vocab.keySet()) {
-			Multiple_Counter word = vocab.get(it);
-			/// Calculate probability of word being in regular
-			if (word.counter_regular > 0) {
-				word.Pregular = Math.log10(word.counter_regular / (float)nWordsRegular);
-			} else {
-				word.Pregular = Math.log10(epsilon / (float)(nWordsRegular + nWordsSpam));
-			}
-			/// Calculate probability of word being in spam
-			if (word.counter_spam > 0) {
-				word.Pspam = Math.log10(word.counter_spam / (float)nWordsSpam);
-			} else {
-				word.Pspam = Math.log10(epsilon / (float)(nWordsRegular + nWordsSpam));
-			}
-		}
-		
-		// 2) The vocabulary must be clean: punctuation and digits must be removed, case insensitive
-		/// See readMessages()
+        System.out.println("P(regular):\t" + Pregular + "\nP(spam):\t" + Pspam);
+
+        for (String it : vocab.keySet()) {
+            Multiple_Counter word = vocab.get(it);
+            nWordsRegular += word.counter_regular;
+            nWordsSpam    += word.counter_spam;
+        }
+
+        for (String it : vocab.keySet()) {
+            Multiple_Counter word = vocab.get(it);
+            /// Calculate probability of word being in regular
+            if (word.counter_regular > 0) {
+                word.Pregular = (float)Math.log10(word.counter_regular / (float)nWordsRegular);
+            } else {
+                word.Pregular = (float)Math.log10(epsilon / (float)(nWordsRegular + nWordsSpam));
+            }
+            /// Calculate probability of word being in spam
+            if (word.counter_spam > 0) {
+                word.Pspam = (float)Math.log10(word.counter_spam / (float)nWordsSpam);
+            } else {
+                word.Pspam = (float)Math.log10(epsilon / (float)(nWordsRegular + nWordsSpam));
+            }
+        }
+
+        // 2) The vocabulary must be clean: punctuation and digits must be removed, case insensitive
+        /// See readMessages()
         // 3) Conditional probabilities must be computed for every word
         // 4) A priori probabilities must be computed for every word
         // 5) Zero probabilities must be replaced by a small estimated value
         // 6) Bayes rule must be applied on new messages, followed by argmax classification
+        /// Start reading the test data
+        int correctRegular = 0, falseRegular = 0, 
+            correctSpam    = 0, falseSpam    = 0;
+        File test = new File(args[1]);
+        if (!test.isDirectory()) {
+            System.out.println("-Error: " + args[1] + " not a directory.\n");
+            Runtime.getRuntime().exit(0);
+        }
+
+        listDirs(test);
+
+        float allMsg = listing_regular.length + listing_spam.length;
+        for (File f : listing_regular) {
+            /// Initialize Pregmsg and Pspammsg to Pregular/Pspam + alpha
+            float Pregmsg  = Pregular + (1/allMsg),
+                  Pspammsg = Pspam    + (1/allMsg);
+            for (String str : readFile(f)) {
+                if (vocab.containsKey(str)) {
+                    Pregmsg  += vocab.get(str).Pregular;
+                    Pspammsg += vocab.get(str).Pspam;
+                }
+            }
+            if (Pregmsg > Pspammsg) {
+                correctRegular++;
+            } else {
+                falseSpam++;
+            }
+        }
+
+        for (File f : listing_spam) {
+            /// Initialize Pregmsg and Pspammsg to Pregular/Pspam + alpha
+            float Pregmsg  = Pregular + (1/allMsg),
+                  Pspammsg = Pspam    + (1/allMsg);
+            for (String str : readFile(f)) {
+                if (vocab.containsKey(str)) {
+                    Pregmsg  += vocab.get(str).Pregular;
+                    Pspammsg += vocab.get(str).Pspam;
+                }
+            }
+            if (Pregmsg > Pspammsg) {
+                falseRegular++;
+            } else {
+                correctSpam++;
+            }
+        }
+
+        float car = correctRegular/(float)allMsg;
+        float crr = correctSpam   /(float)allMsg;
+        float far = falseRegular  /(float)allMsg;
+        float frr = falseSpam     /(float)allMsg;
+        System.out.println("Correct accept rate:\t"+car);
+        System.out.println("Correct reject rate:\t"+crr);
+        System.out.println("False   accept rate:\t"+far);
+        System.out.println("False   reject rate:\t"+frr);
+
         // 7) Errors must be computed on the test set (FAR = false accept rate (misses), FRR = false reject rate (false alarms))
         // 8) Improve the code and the performance (speed, accuracy)
         //
