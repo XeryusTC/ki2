fbe7185c8e810c23fdb95c5eef65cc4fc407c82d
rmellema <rene.mellema@gmail.com>
2015-03-07 15:24:35 +0100

    Reformatted the code

--- cluster/src/ClusteringAlgorithm.java
+++ cluster/src/ClusteringAlgorithm.java
@@ -1,14 +1,13 @@
-public abstract class ClusteringAlgorithm
-{
-	public abstract boolean train();
+public abstract class ClusteringAlgorithm {
+    public abstract boolean train();
 
-	public abstract boolean test();
-	
-	public abstract void setPrefetchThreshold(double prefetchThreshold);    
-	
-	public abstract void showTest();
-	
-	public abstract void showMembers();
-	
-	public abstract void showPrototypes();
+    public abstract boolean test();
+
+    public abstract void setPrefetchThreshold(double prefetchThreshold);
+
+    public abstract void showTest();
+
+    public abstract void showMembers();
+
+    public abstract void showPrototypes();
 }
